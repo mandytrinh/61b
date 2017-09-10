f30c043ad6cd52d3c29c2b0170979ead2b4010c5
Trinh <TrinhM@medamerica.com>
2017-09-10 09:55:58 -0700

    almost done with lab 2 and started on proj 0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -6,5 +6,29 @@ public class Planet
     public double yyVel;
     public double mass;
     String imgFileName;
-    
+
+    //first constructor
+    public Planet(double xP, double yP, double xV,
+                  double yV, double m, String img)
+    {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+
+    }
+    //second constructor: should take in a Planet object and initialize an identical Planet object
+    public Planet(Planet p)
+    {
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+
+    }
+
 }
