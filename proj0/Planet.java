0a6e6df6485746295d802cae61229045b4598e5e
Trinh <TrinhM@medamerica.com>
2017-09-16 23:14:18 -0700

    half way done with update method

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -6,6 +6,12 @@ public class Planet
     public double yyVel;
     public double mass;
     String imgFileName;
+    public double netAcceleration_X;
+    public double netAcceleration_Y;
+    public double xNewVelocity;
+    public double yNewVelocity; 
+    public double xNewPos;
+    public double yNewPos;
 
     //first constructor
     public Planet(double xxPos, double yyPos, double xxVel,
@@ -17,9 +23,9 @@ public class Planet
         this.yyVel = yyVel;
         this.mass = mass;
         this.imgFileName = imgFileName;
-
     }
-    //second constructor: should take in a Planet object and initialize an identical Planet object
+    /* second constructor: should take in a Planet object and 
+    initialize an identical Planet object */
     public Planet(Planet p) //p can be replaced with any name for Planet object
     {
         this.xxPos = p.xxPos;
@@ -28,12 +34,12 @@ public class Planet
         this.yyVel = p.yyVel;
         this.mass = p.mass;
         this.imgFileName = p.imgFileName;
-
     }
     
 	public double calcDistance(Planet exertingPlanet)
 	{ 
-    /*This method will take in a single Planet and should return a double equal to the distance between the supplied planet and the planet that is doing the calculation
+    /* This method will take in a single Planet and should return a double equal 
+    to the distance between the supplied planet and the planet that is doing the calculation
     */
 		double distance; //r = distance
 		double rSquared;
@@ -48,7 +54,9 @@ public class Planet
     
     public double calcForceExertedBy(Planet exertingPlanet)
     {
-    /* Pairwise force: This method takes in a planet (what's doing the exertion) and returns a double describing the force exerted on this planet by the given planet. Call the calcDistance method here. */
+    /* Pairwise force: This method takes in a planet (what's doing the exertion) 
+    and returns a double describing the force exerted on this planet by the given planet.
+    Call the calcDistance method here. */
     double graviationalConstant =  0.0000000000667;
     double mass1 = this.mass;
     double mass2 = exertingPlanet.mass;
@@ -80,7 +88,8 @@ public class Planet
     
     public double calcNetForceExertedByX(Planet[] allExertingPlanets)
     { 
-    /* NET X FORCE: This method takes in an array of Planets and calculates the net X force exerted by all planets in that array upon the current Planet */
+    /* NET X FORCE: This method takes in an array of Planets and calculates the net X force 
+    exerted by all planets in that array upon the current Planet */
         double force_X = 0;
         double netX = 0;
         for (Planet currentPlanet : allExertingPlanets)
@@ -96,7 +105,8 @@ public class Planet
     
     public double calcNetForceExertedByY(Planet[] allExertingPlanets)
     { 
-    /* NET Y FORCE: This method takes in an array of Planets and calculates the net Y force exerted by all planets in that array upon the current Planet */
+    /* NET Y FORCE: This method takes in an array of Planets and calculates the net Y force 
+    exerted by all planets in that array upon the current Planet */
         double force_Y = 0;
         double netY = 0;
         for (Planet currentPlanet : allExertingPlanets)
@@ -110,6 +120,27 @@ public class Planet
         return netY; 
     }
 
-    
+    public void update(double dt, double netX, double netY)
+    {
+    /* determines how much the forces exerted on the planet will cause that planet to accelerate,
+    & the resulting change in the planet's velocity and position in a small period of time dt. 
+    I.e this updates the planet's position and velocity instance variables */
+        
+        // 1) Calculate the acceleration using the provided x and y forces 
+        this.netAcceleration_X = netX/this.mass;
+        this.netAcceleration_Y = netY/this.mass;
+        
+        /* 2) Calculate the new velocity by using the acceleration and current velocity. 
+        Recall that accleration describes the change in velocity per unit time, 
+        so the new velocity is (vx + dt * ax, vy + dt * ay) */
+        this.xxVel = this.xxVel + dt * this.netAcceleration_X;
+        this.yyVel = this.yyVel + dt * this.netAcceleration_Y;
+        
+        /* 3) Calculate the new position by using the velocity computed in step 2 
+        and the current position. The new position is (px + dt * vx, py + dt * vy) */
+        this.xxPos = this.xxPos + dt * this.xNewVelocity;
+        this.yyPos = this.yyPos + dt * this.yNewVelocity;
+        
+    }
     
 }
