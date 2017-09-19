7ab1f925d1e318fc685152ca763ff4a44f39be44
Trinh <TrinhM@medamerica.com>
2017-09-18 22:00:23 -0700

    added NBody class, completed readradius method

---
+++ proj0/NBody.java
@@ -0,0 +1,25 @@
+/* This class will have NO constructor. 
+    The goal of this class is to simulate a universe specified in one of the data files */
+    
+import java.util.Scanner;
+public class NBody
+{
+    //public static void main(String[] args)
+
+    public static double readRadius(String fileName)
+    { /*  Given a file name, it should return a double corresponding to the radius of the universe in that file 
+    Note - This is a CLASS method (static)
+    It is not invoked on an instant but on the class itself */
+    
+        In in = new In(fileName);
+        int numOfPlanets = 0;
+        double radius = 0;        
+        while (!in.isEmpty())
+        {
+            numOfPlanets = in.readInt();
+            radius = in.readDouble();
+            break;
+        }
+        return radius;        
+    }
+}
