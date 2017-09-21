097f41fa315e758ecce65752584981ecb68fb0d1
Trinh <TrinhM@medamerica.com>
2017-09-20 22:42:46 -0700

    added readplanets method;

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -4,8 +4,6 @@
 import java.util.Scanner;
 public class NBody
 {
-    //public static void main(String[] args)
-
     public static double readRadius(String fileName)
     { /*  Given a file name, it should return a double corresponding to the radius of the universe in that file 
     
@@ -22,5 +20,36 @@ public class NBody
             break;
         }
         return radius;        
+    }
+    
+    public static double readPlanets(String fileName)
+    { /* Given a file name, it should return an array of Planets corresponding to the planets in the file */
+    
+        In in = new In(fileName);
+        int numOfPlanets = in.readInt();
+        double radius = in.readDouble();
+        Planet[] planetsArray = new Planet[numOfPlanets];      
+        double xVel = 0;
+        double yVel = 0;
+        double xPos = 0;
+        double yPos = 0;
+        double mass = 0;
+        String planetName = "";
+        int i = 0;
+        while (i < numOfPlanets)
+        {
+            // read xvel, yvel, xpos, ypos, mass, name
+            Planet p = new Planet(double xPos, double yPos, double xVel, double yVel, double mass, String planetName);
+            planetsArray[i] = p;
+            i = i + 1;
+            xPos = in.readDouble;
+            yPos = in.readDouble;
+            xVel = in.readDouble;
+            yVel = in.readDouble;
+            mass = in.readDouble;
+            planetName = in.readString;            
+        }
+        
+        
     }
 }
