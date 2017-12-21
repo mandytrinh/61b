5c63a795f7563f9befafe1d0220becab71748c98
Mandy Trinh <saphiric@gmail.com>
2017-12-21 13:02:17 -0800

    added getcapacity method, space formatting

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -19,20 +19,13 @@ public class ArrayDeque<genericType>
         frontPointer = 0;
     }
 
-    private boolean arrayIsFull()
-    {
-        return size == capacity;
-    }
+	private int getCapacity() { return capacity; }
 
-    public int size()
-    {
-        return size;
-    }
+	private boolean arrayIsFull() { return size == capacity; }
 
-    public boolean isEmpty()
-    {
-        return size == 0;
-    }
+    public int size() {return size; }
+
+    public boolean isEmpty() { return size == 0; }
 
 	public void maintainUsageFactor()
 	{
