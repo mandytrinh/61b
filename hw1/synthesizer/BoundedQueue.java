2d5cf76878cd0a67e1868eded1388bb02c434a52
Mandy Trinh <saphiric@gmail.com>
2018-04-20 20:46:11 -0700

    added BoundedQueue and AbstractBoundedQueue for guitar synthesizer

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,12 @@
+package synthesizer;
+
+public interface BoundedQueue<T>
+{
+    int capacity();
+    int fillCount();
+    void enqueue(T x);
+    T dequeue();
+    T peek();
+    boolean isEmpty();
+    boolean isFull();
+}
